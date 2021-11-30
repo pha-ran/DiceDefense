abbfa98c4e61237b8b4a3d610cdc8d92fcd83c5c
pha-ran <pha-ran@naver.com>
2021-11-30 19:06:20 +0900

    Integrate Thread

--- app/src/main/java/com/dicedefense/Game/GameActivity.kt
+++ app/src/main/java/com/dicedefense/Game/GameActivity.kt
@@ -5,25 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicedefense.Main.ScoreActivity
 import com.dicedefense.databinding.ActivityGameBinding
-import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.concurrent.timer
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding : ActivityGameBinding
-    private val random = Random()
-    private var hp = 100
-    private var score = 0
-    private var gold = 50
-    private val enemyList = ArrayList<Enemy>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        gameLoop()
-        respawnEnemy()
     }
 
     // 뒤로가기 키 방지
@@ -31,77 +20,11 @@ class GameActivity : AppCompatActivity() {
         //super.onBackPressed()
     }
 
-    private fun gameLoop() {
-        // 0.01초 단위
-        timer(period = 10) {
-            deleteEnemy()
-            moveEnemy()
-
-            if (hp <= 0) {
-                cancel()
-
-                // ScoreActivity 실행 후 GameActivity 종료
-                val intent = Intent(this@GameActivity, ScoreActivity::class.java)
-                intent.putExtra("score", score)
-                startActivity(intent)
-                finish()
-            }
-
-            runOnUiThread {
-                binding.textView.text = "Gold : $gold" // gold 출력
-            }
-        }
-    } // gameLoop()
-
-    private fun respawnEnemy() {
-        // 1초 단위
-        timer(period = 1000, initialDelay = 3000) {
-            enemyList.add(Enemy(50f, 0f, 100)) // 적 객체 생성
-
-            if (hp <= 0) {
-                cancel()
-            }
-        }
-    } // respawnEnemy()
-
-    fun getHp() : String {
-        return "HP : $hp" // GameView에서 hp사용
+    fun gameOver() {
+        // ScoreActivity 실행 후 GameActivity 종료
+        val intent = Intent(this@GameActivity, ScoreActivity::class.java)
+        intent.putExtra("score", binding.gameView.getScore())
+        startActivity(intent)
+        finish()
     }
-
-    fun getScore() : String {
-        return "score : $score" // GameView에서 score사용
-    }
-
-    fun getEnemyList() : ArrayList<Enemy> {
-        return enemyList // GameView에서 enemyList사용
-    }
-
-    private fun moveEnemy() {
-        // 모든 적 객체를 이동
-        for (e in enemyList) {
-            e.y += 3 // y좌표 아래로 이동
-
-            // hp 감소시 삭제 test
-            if (random.nextInt(150)== 50) {
-                e.hp -= 50
-            }
-        }
-    } // moveEnemy()
-
-    private fun deleteEnemy() {
-        // enemy 객체의 현재 hp가 0이하가 되거나 bottom 좌표를 초과할 경우 삭제
-        val iterator = enemyList.iterator()
-        while (iterator.hasNext()) {
-            val e = iterator.next()
-            if (e.hp <= 0) { // hp가 0이 이하가 될 경우
-                iterator.remove()
-                gold += 10 // 적 처치 시 골드 획득
-                score += 10 // 적 처치 시 점수 증가
-            }
-            if (e.y > binding.gameView.getBot()) { // bottom 좌표를 초과할 경우
-                iterator.remove()
-                hp -= 10 // 적 처치 실패 시 hp 감소
-            }
-        }
-    } // deleteEnemy()
 }
--- app/src/main/java/com/dicedefense/Game/GameView.kt
+++ app/src/main/java/com/dicedefense/Game/GameView.kt
@@ -6,19 +6,30 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import java.util.Random
+import kotlin.collections.ArrayList
 
 class GameView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
-    private var gameActivity = (context as GameActivity)
-    private var x = 0
-    private var y = 0
-    private var top = 0f
-    private var mid = 0f
-    private var bot = 0f
+    // Val
+    private val gameActivity = (context as GameActivity)
     private val paintBlack = Paint()
     private val paintWhite = Paint()
     private val paintGray = Paint()
     private val paintBlue = Paint()
     private val paintRed = Paint()
+    private val random = Random()
+    private val enemyList = ArrayList<Enemy>()
+
+    // Var
+    private var x = 0
+    private var y = 0
+    private var top = 0f
+    private var mid = 0f
+    private var bot = 0f
+    private var playerHp = 100
+    private var score = 0
+    private var gold = 50
+    private var time = 0
 
     // Initialize
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
@@ -40,33 +51,68 @@ class GameView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        initialDraw(canvas)
-        enemyDraw(canvas)
+        if (playerHp <= 0) { // hp 가 0 이하가 될 경우 종료
+            gameActivity.gameOver()
+        }
+        else {
+            initialDraw(canvas)
 
-        invalidate()
+            time += 1
+            if (time >= 50) { // 초당 1회 동작
+                enemyList.add(Enemy(50f, 0f, 100)) // 적 객체 생성
+                time = 0
+            }
+
+            enemyDraw(canvas)
+            
+            invalidate()
+        }
     }
 
     private fun initialDraw(canvas: Canvas) {
+        // 기본 도형 그리기
         canvas.drawRect(mid - 5, top, mid + 5, bot, paintGray)
         canvas.drawRect(mid - 60, top, mid + 60, top + 10, paintBlue)
         canvas.drawRect(mid - 60, bot - 10, mid + 60, bot, paintRed)
 
-        canvas.drawText(gameActivity.getScore(), x/16f, y/16f, paintBlack) // score 출력
-        canvas.drawText(gameActivity.getHp(), x/16f, 15*y/16f, paintBlack) // player 의 hp 출력
+        // 현재 상태 그리기
+        canvas.drawText("score : $score", x/16f, y/16f, paintBlack)
+        canvas.drawText("HP : $playerHp", x/16f, 15*y/16f, paintBlack)
+        canvas.drawText("gold : $gold", 10*x/16f, 15*y/16f, paintBlack)
     }
 
-    private fun enemyDraw(canvas: Canvas) {
-        val enemyList = gameActivity.getEnemyList()
-
+    @Synchronized private fun enemyDraw(canvas: Canvas) {
         // enemyList 의 enemy 객체들을 모두 Draw
         for (enemy in enemyList) {
             val size = enemy.size
             canvas.drawRect(mid - size, enemy.y - size, mid + size, enemy.y + size, paintBlack)
             canvas.drawText(enemy.hp.toString(), mid, enemy.y + 20, paintWhite) // enemy 객체의 hp 출력
         }
+
+        val iterator = enemyList.iterator()
+        while (iterator.hasNext()) {
+            val e = iterator.next()
+            if (e.hp <= 0) { // hp가 0이 이하가 될 경우
+                iterator.remove()
+                gold += 10 // 적 처치 시 골드 획득
+                score += 10 // 적 처치 시 점수 증가
+            }
+            else if (e.y > bot) { // bottom 좌표를 초과할 경우
+                iterator.remove()
+                playerHp -= 10 // 적 처치 실패 시 hp 감소
+            }
+            else { // 삭제 조건이 아닐 경우 객체 이동
+                e.y += 5 // y좌표 아래로 이동
+
+                // hp 감소시 삭제 test
+                if (random.nextInt(150)== 50) {
+                    e.hp -= 100
+                }
+            }
+        }
     }
 
-    fun getBot() : Float {
-        return bot // GameActivity 에서 bot 사용
+    fun getScore() : Int {
+        return score // GameActivity 에서 score 사용
     }
 }
